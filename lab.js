03c4d8e2a1d370c26d03837fcc607608fc329af9
Kylec94 <kyleianc94@gmail.com>
2022-03-23 15:06:29 -0600

    Initial commit

---
+++ lab.js
@@ -0,0 +1,62 @@
+// let lovesCode = true;
+
+// if(lovesCode === true){
+//   console.log("I love to code!")
+// } else{
+//   console.log("Coding has its challenges")
+// }
+
+
+// let amysAge = 29;
+// let brittanisAge = 34;
+// let amysBirthYear = 1991;
+// let brittanisBirthYear = 1986;
+
+// if (amysAge > brittanisAge) {
+//   console.log('Amy is older than Brittani')
+// } else if (brittanisAge > amysAge) {
+//   console.log('Brittani is older than Amy')
+// } else {
+//   console.log('Amy and Brittani are the same age.')
+// }
+
+// if(amysBirthYear === brittanisBirthYear){
+//   console.log("Amy and Brittany were born in the same year")
+// } else {
+//   console.log("Amy and Brittany were not born in the same year")
+// }
+
+// let temperature = 55;
+// let rain = true;
+
+// if(temperature > 80 && rain){
+//   console.log("Wear normal clothes but don't bring an umbrella because rain is amazing")
+// } else if(temperature > 60 && rain) {
+//   console.log("Still don't wear a rain jacket or umbrella because rain is amazing")
+// } else if(temperature < 60 && rain) {
+//   console.log("celebrate and go for a run because this is perfect weather")
+// }
+
+// for (let i = 0; i < 10; i++){
+//   console.log("hello world")
+// }
+
+// for (let i = 1; i < 11; i++) {
+//   console.log(i)
+// }
+
+// for(let i = 10; i > 0; i--){
+//   console.log(i)
+// }
+
+// let score = 0;
+// let passingScore = 7;
+
+// while(score < passingScore){
+//   console.log("Your score is not high enough")
+//   score++
+// }
+
+
+
+
